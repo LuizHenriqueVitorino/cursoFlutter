bb20f4a5e107376b67d1dee127365df3fc76ffb5
LuizHenriqueVitorino <henriquefisicoquimico@gmail.com>
2025-11-10 14:10:06 -0300

    feat: adiciona a primeira pergunta e as primeiras respostas ao app de perguntas

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -7,10 +7,61 @@ void main() {
 class PerguntaAP extends StatelessWidget{
   const PerguntaAP({super.key});
 
+  void responder(){
+    print('Pergunta respondida');
+  }
+
+  void Function() resposta(String resposta){
+    return () {
+      print('A resposta está $resposta');
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a cor favorita do Luiz?',
+      'Qual é o animal favorito do Luiz?'
+    ];
     return MaterialApp(
-      home: Text('Olá mundo!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas do Luiz', 
+            style: TextStyle(color: Color(0xFFFFFFFF))),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 24, 58, 119),
+
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            TextButton(
+              onPressed: resposta('CORRETA'), 
+              style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 0, 0, 255),
+                foregroundColor: Colors.white
+              ),
+              child: Text('Azul')
+              ),
+            TextButton(
+              onPressed: resposta('INCORRETA'), 
+              style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 255, 0, 0),
+                foregroundColor: Colors.white
+              ),
+              child: Text('Vermelho')
+              ),
+            TextButton(
+              onPressed: resposta('INCORRETA'), 
+              style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 0, 255, 0),
+                foregroundColor: Colors.white
+              ),
+              child: Text('Verde')
+              )
+          ],
+        ),
+      ),
     );
   }
 }
